ad7b0a817b803d31f1bf7564b606a1854fe4cdbf
Ammar <ammar.saeed@visma.com>
2024-06-09 17:17:59 +0500

    Initial Commit

---
+++ Image PuzzleUITests/Image_PuzzleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Image_PuzzleUITestsLaunchTests.swift
+//  Image PuzzleUITests
+//
+//  Created by Arkhitech on 09/06/2024.
+//
+
+import XCTest
+
+final class Image_PuzzleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
